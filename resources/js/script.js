c17538e85445f82ddcaa8ca84440d938b1763ad2
barros.daniel@outlook.com <barros.daniel@outlook.com>
2018-11-29 07:14:53 +0800

    feature: sticky navigation script completed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,16 @@
-$(document).ready(function(){
-
-  $('h1').click(function(){
-    $(this).css('background-color', 'red');
-  });
-  
+$('.js--section-features').waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.nav-bar-container').addClass('sticky');
+  } else {
+    $('.nav-bar-container').removeClass('sticky');
+  }
+}, {
+  offset: '60px'
 });
+
+// Snippet from Waypoints documentation
+// var waypoints = $('#handler-first').waypoint(function(direction) {
+//   notify(this.element.id + ' hit 25% from top of window') 
+// }, {
+//   offset: '25%'
+// })
